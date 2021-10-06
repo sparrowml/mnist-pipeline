a5eb7fec0373deb929fb3095de048eb6a7e8809c
Ben Cook <ben@sparrow.dev>
2021-10-06 21:04:32 +0000

    no sweeps for now

--- mnist/sweep.py
+++ mnist/sweep.py
@@ -1,21 +1,21 @@
-import wandb
-import yaml
+# import wandb
+# import yaml
 
-from .config import MnistConfig
-from .train import train_model
-
-
-def start_sweep(config: str) -> str:
-    with open(config) as f:
-        sweep_config = yaml.safe_load(f.read())
-    sweep_id = wandb.sweep(sweep_config)
-    return sweep_id
+# from .config import MnistConfig
+# from .train import train_model
 
 
-def launch_agent(sweep_id: str, count: int = 3) -> None:
-    def train() -> None:
-        with wandb.init():
-            config = MnistConfig(**wandb.config)
-            train_model(config.learning_rate, config.max_epochs, config.batch_size)
+# def start_sweep(config: str) -> str:
+#     with open(config) as f:
+#         sweep_config = yaml.safe_load(f.read())
+#     sweep_id = wandb.sweep(sweep_config)
+#     return sweep_id
 
-    wandb.agent(sweep_id, function=train, count=count)
+
+# def launch_agent(sweep_id: str, count: int = 3) -> None:
+#     def train() -> None:
+#         with wandb.init():
+#             config = MnistConfig(**wandb.config)
+#             train_model(config.learning_rate, config.max_epochs, config.batch_size)
+
+#     wandb.agent(sweep_id, function=train, count=count)
