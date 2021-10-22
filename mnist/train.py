9a93798dd7905adacf84547d81c66d4ee178ae5f
jbencook <jbencook@protonmail.com>
2021-10-22 15:19:32 +0000

    simplify train

--- mnist/train.py
+++ mnist/train.py
@@ -15,8 +15,7 @@ class MnistLightning(pl.LightningModule):
         self._learning_rate = learning_rate
         self.model = MnistClassifier()
         self.loss = torch.nn.CrossEntropyLoss()
-        self.train_accuracy = torchmetrics.Accuracy()
-        self.dev_accuracy = torchmetrics.Accuracy()
+        self.accuracy = torchmetrics.Accuracy()
 
     def training_step(
         self, batch: Tuple[torch.Tensor, torch.Tensor], _
@@ -24,16 +23,18 @@ class MnistLightning(pl.LightningModule):
         x, y = batch
         yhat = self.model(x)
         loss = self.loss(yhat, y)
-        acc = self.train_accuracy(torch.softmax(yhat, -1), y)
-        self.log("train_loss", loss)
-        self.log("train_accuracy", acc, prog_bar=True, on_step=True)
+        self.log("loss", loss)
         return loss
 
     def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], _) -> None:
         x, y = batch
         yhat = self.model(x)
-        acc = self.dev_accuracy(torch.softmax(yhat, -1), y)
-        self.log("dev_accuracy", acc, prog_bar=True, on_epoch=True)
+        self.log(
+            "accuracy",
+            self.accuracy(torch.softmax(yhat, -1), y),
+            prog_bar=True,
+            on_epoch=True,
+        )
 
     def configure_optimizers(self):
         return torch.optim.Adagrad(self.parameters(), lr=self._learning_rate)
@@ -43,22 +44,14 @@ def train_model(
     learning_rate: float = MnistConfig.learning_rate,
     max_epochs: int = MnistConfig.max_epochs,
     batch_size: int = MnistConfig.batch_size,
-    progress_bar: bool = False,
+    random_seed: int = MnistConfig.random_seed,
+    feature_weights_path: str = MnistConfig.feature_weights_path,
 ) -> None:
     """Train the model and save classifier and feature weights."""
-    config = MnistConfig(
-        learning_rate=learning_rate,
-        max_epochs=max_epochs,
-        batch_size=batch_size,
-    )
-    pl.utilities.seed.seed_everything(config.random_seed)
-    trainer = pl.Trainer(
-        checkpoint_callback=False,
-        max_epochs=config.max_epochs,
-        progress_bar_refresh_rate=None if progress_bar else 0,
-    )
-    train_loader = load_dataset(batch_size=config.batch_size)
-    dev_loader = load_dataset(train=False, batch_size=config.batch_size)
-    pl_model = MnistLightning(config.learning_rate)
+    pl.utilities.seed.seed_everything(random_seed)
+    trainer = pl.Trainer(max_epochs=max_epochs)
+    train_loader = load_dataset(batch_size=batch_size)
+    dev_loader = load_dataset(train=False, batch_size=batch_size)
+    pl_model = MnistLightning(learning_rate)
     trainer.fit(pl_model, train_loader, dev_loader)
-    torch.save(pl_model.model.features, config.feature_weights_path)
+    torch.save(pl_model.model.features, feature_weights_path)
